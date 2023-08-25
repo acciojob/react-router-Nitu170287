b0fa3eebd4749212f96973c31a6924250adf90fc
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-25 10:14:38 -0400

    change the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,10 @@
 import React from "react";
-const About =()=>{
-
-
-    return(
-        <div>
-        <h1>About</h1>
-        <p>This is a sample react router program.</p>
-        </div>
-    )
-}
-export default About
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+};
+export default About;
